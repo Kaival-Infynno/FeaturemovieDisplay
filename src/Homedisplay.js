abce655c0a160cb0ee4c53ad968bebbf532a60ab
Kaival-Infynno <kaival.infynno@gmail.com>
2022-02-12 01:45:19 +0530

    after carousal

--- src/Homedisplay.js
+++ src/Homedisplay.js
@@ -16,11 +16,11 @@ import VFacebook from "./images/VectorFacebook.png";
 import VInstagram from "./images/Vectorinsta.png";
 import VTwitter from "./images/VectorTwiter.png";
 import VYoutube from "./images/Vectoryoutube.png";
-import moment from "moment";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 import ReactPlayer from "react-player";
+import moment from "moment";
+import Carousel from "react-elastic-carousel";
 import "./input.css";
+
 const Homedisplay = () => {
   const dispatch = useDispatch();
   const { letestMovieData } = useSelector((state) => ({
@@ -36,6 +36,15 @@ const Homedisplay = () => {
     allCast: state.movieData.allCast,
   }));
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 4, itemsToScroll: 4 },
+  ];
+  const breakPoints1 = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 3, itemsToScroll: 3 },
+  ];
+
   useEffect(() => {
     dispatch(getMovieData());
     dispatch(getUCMovieData());
@@ -45,7 +54,7 @@ const Homedisplay = () => {
 
   return (
     <div>
-      <div className="flex flex-row md:justify-between justify-around xl:pl-24 2xl:pl-32 lg:pl-10 md:pl-12 pl-4 xl:pr-24 2xl:pr-32 lg:pr-6 md:pr-10 pr-2 mt-16">
+      <div className="flex flex-row md:justify-between justify-around xl:pl-28 2xl:pl-32 lg:pl-20 md:pl-20 pl-4 xl:pr-28 2xl:pr-28 lg:pr-20 md:pr-20 pr-2 lg:mt-16 md:mt-6 mt-4">
         <div className="mainTitle lg:mb-11 mb-4">Featured Movie</div>
         <div className="flex flex-row py-3">
           <div className="navsee pr-2">See more </div>
@@ -54,78 +63,59 @@ const Homedisplay = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex flex-row ">
-        {/* <div>
-          <img
-            src={leftnav}
-            className="  mt-32  hidden md:block  lg:w-12 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4 "
-            alt="not found"
-          />
-        </div> */}
-        <div
-          id="carouselExampleControls"
-          class="carousel slide relative"
-          data-bs-ride="carousel"
+      <div className="flex">
+        <Carousel
+          breakPoints={breakPoints}
+          pagination={false}
+          renderArrow={myArrow}
+          className="mx-5"
         >
-          <div class="carousel-inner relative w-full overflow-hidden">
-            <div className="flex md:flex-row flex-col xl:space-x-20 lg:space-x-6 md:space-x-4 space-x-0">
-              {letestMovieData &&
-                letestMovieData.results &&
-                letestMovieData.results.map((res) => (
-                  <div key={res.id}>
-                    <div className="relative sm:flex-col xl:w-64 lg:w-56 md:w-40">
-                      <img
-                        src={`http://image.tmdb.org/t/p/w185/${res.poster_path}`}
-                        className="lg:h-96 md:h-72 w-64"
-                        alt="not found"
-                      />
+          {letestMovieData &&
+            letestMovieData.results &&
+            letestMovieData.results.map((res) => (
+              <div key={res.id}>
+                <div className="relative sm:flex-col xl:w-64 lg:w-48 md:w-36 w-48">
+                  <img
+                    src={`http://image.tmdb.org/t/p/w185/${res.poster_path}`}
+                    className="xl:h-96 lg:h-72 md:h-64 w-64"
+                    alt="not found"
+                  />
 
-                      <div className="absolute top-4 right-4">
-                        <img src={Favorite} alt="not found" />
-                      </div>
+                  <div className="absolute top-4 right-4">
+                    <img src={Favorite} alt="not found" />
+                  </div>
+                </div>
+                <div className="pt-3 dateInside">
+                  {moment(res.release_date).format("YYYY")}
+                </div>
+                <div className="insideTitle xl:w-64 lg:w-48 md:w-36 w-48">
+                  {res.title}
+                </div>
+                <div className="pt-3 flex flex-row justify-between xl:w-64 lg:w-48 md:w-36 w-48">
+                  <div className="flex lg:flex-row md:flex-col">
+                    <div>
+                      <img src={imdb} alt="not found" />
                     </div>
-                    <div className="pt-3 dateInside">
-                      {moment(res.release_date).format("YYYY")}
-                    </div>
-                    <div className="insideTitle xl:w-64 lg:w-56 md:w-40 w-64">
-                      {res.title}
-                    </div>
-                    <div className="pt-3 flex flex-row justify-between xl:w-64 lg:w-56 md:w-40 w-64">
-                      <div className="flex lg:flex-row md:flex-col">
-                        <div>
-                          <img src={imdb} alt="not found" />
-                        </div>
-                        <div className="imdbrating lg:pl-2.5 lg:py-0.5 md:py-2">
-                          {res.vote_average * 10} / 100
-                        </div>
-                      </div>
-                      <div className="flex flex-row pb-4 md:pb-0">
-                        <div>
-                          <img src={apple} alt="not found" />
-                        </div>
-                        <div className="imdbrating py-0.5 pl-2.5">
-                          {res.popularity.toFixed(2)}
-                        </div>
-                      </div>
+                    <div className="imdbrating lg:pl-2.5 lg:py-0.5 md:py-2">
+                      {res.vote_average * 10} / 100
                     </div>
                   </div>
-                ))}
-            </div>
-          </div>
-        </div>
-
-        {/* <div>
-          <img
-            src={rightNav}
-            className="lg:w-10 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4   mt-32 hidden md:block"
-            alt="not found"
-          />
-        </div> */}
+                  <div className="flex flex-row pb-4 md:pb-0">
+                    <div>
+                      <img src={apple} alt="not found" />
+                    </div>
+                    <div className="imdbrating py-0.5 pl-2.5">
+                      {res.popularity.toFixed(2)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </Carousel>
       </div>
 
       {/* upComing */}
-      <div className="flex flex-row md:justify-between justify-around xl:pl-24 2xl:pl-32 lg:pl-10 md:pl-12 pl-4 xl:pr-24 2xl:pr-32 lg:pr-6 md:pr-10 pr-2 mt-16">
+      <div className="flex flex-row md:justify-between justify-around xl:pl-28 2xl:pl-32 lg:pl-20 md:pl-20 pl-4 xl:pr-28 2xl:pr-28 lg:pr-20 md:pr-20 pr-2 lg:mt-16 md:mt-6 mt-4">
         <div className="mainTitle lg:mb-11 mb-4">New Arrival</div>
         <div className="flex flex-row py-3">
           <div className="navsee pr-2">See more </div>
@@ -135,25 +125,24 @@ const Homedisplay = () => {
         </div>
       </div>
 
-      <div className="flex flex-row justify-center">
-        <div>
-          <img
-            src={leftnav}
-            className="  mt-32  hidden md:block  lg:w-12 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4 "
-            alt="not found"
-          />
-        </div>
-        <div className="flex md:flex-row flex-col xl:space-x-20 lg:space-x-6 md:space-x-4 space-x-0">
+      <div className="flex">
+        <Carousel
+          breakPoints={breakPoints}
+          pagination={false}
+          renderArrow={myArrow}
+          className="mx-5"
+        >
           {upcomingMdata &&
             upcomingMdata.results &&
-            upcomingMdata.results.slice(8, 12).map((rdata) => (
+            upcomingMdata.results.map((rdata) => (
               <div key={rdata.id}>
-                <div className="relative sm:flex-col xl:w-64 lg:w-56 md:w-40">
+                <div className="relative sm:flex-col xl:w-64 lg:w-48 md:w-36 w-48">
                   <img
                     src={`http://image.tmdb.org/t/p/w185/${rdata.poster_path}`}
-                    className="lg:h-96 md:h-72 w-64"
+                    className="xl:h-96 lg:h-72 md:h-64 w-64"
                     alt="not found"
                   />
+
                   <div className="absolute top-4 right-4">
                     <img src={Favorite} alt="not found" />
                   </div>
@@ -161,10 +150,10 @@ const Homedisplay = () => {
                 <div className="pt-3 dateInside">
                   {moment(rdata.release_date).format("YYYY")}
                 </div>
-                <div className="insideTitle xl:w-64 lg:w-56 md:w-40 w-64">
+                <div className="insideTitle xl:w-64 lg:w-48 md:w-36 w-48">
                   {rdata.title}
                 </div>
-                <div className="pt-3 flex flex-row justify-between xl:w-64 lg:w-56 md:w-40 w-64">
+                <div className="pt-3 flex flex-row justify-between xl:w-64 lg:w-48 md:w-36 w-48">
                   <div className="flex lg:flex-row md:flex-col">
                     <div>
                       <img src={imdb} alt="not found" />
@@ -184,17 +173,10 @@ const Homedisplay = () => {
                 </div>
               </div>
             ))}
-        </div>
-        <div>
-          <img
-            src={rightNav}
-            className="lg:w-10 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4   mt-32 hidden md:block"
-            alt="not found"
-          />
-        </div>
+        </Carousel>
       </div>
       {/* vidios */}
-      <div className="flex flex-row md:justify-between justify-around xl:pl-24 2xl:pl-32 lg:pl-10 md:pl-12 pl-4 xl:pr-24 2xl:pr-32 lg:pr-6 md:pr-10 pr-2 mt-16">
+      <div className="flex flex-row md:justify-between justify-around xl:pl-28 2xl:pl-32 lg:pl-20 md:pl-20 pl-4 xl:pr-28 2xl:pr-28 lg:pr-20 md:pr-20 pr-2 lg:mt-16 md:mt-6 mt-4">
         <div className="mainTitle lg:mb-11 mb-4">Exclusive Videos</div>
         <div className="flex flex-row py-3">
           <div className="navsee pr-2">See more </div>
@@ -203,20 +185,18 @@ const Homedisplay = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-center">
-        <div>
-          <img
-            src={leftnav}
-            className="  mt-24  hidden md:block  lg:w-10 xl:w-12 md:w-6 xl:mr-6 lg:mr-0 xl:ml-4 md:mr-2"
-            alt="not found"
-          />
-        </div>
-        <div className="flex md:flex-row flex-col xl:space-x-12 lg:space-x-8 md:space-x-4 space-x-0 ">
+      <div className="flex">
+        <Carousel
+          breakPoints={breakPoints1}
+          pagination={false}
+          renderArrow={myArrow}
+          className="mx-5"
+        >
           {allVidios &&
             allVidios.results &&
-            allVidios.results.slice(0, 3).map((vdata) => (
+            allVidios.results.map((vdata) => (
               <div key={vdata.id}>
-                <div className=" sm:flex-col pt-4 md:pt-0 ">
+                <div className=" sm:flex-col pt-4 md:pt-0 xl:w-96 lg:w-64 md:w-54 w-48">
                   <ReactPlayer
                     url={`https://www.youtube.com/watch?v=${vdata.key}`}
                     width="100%"
@@ -224,66 +204,47 @@ const Homedisplay = () => {
                     controls={true}
                   ></ReactPlayer>
                 </div>
-
                 <div className="insideTitle xl:w-96 lg:w-56 md:w-40 w-44">
                   {vdata.type}
                 </div>
               </div>
             ))}
-        </div>
-        <div>
-          <img
-            src={rightNav}
-            className="lg:w-10 xl:w-12 md:w-6 xl:mr-6 lg:mr-0 xl:ml-4 md:mr-2 mt-24 hidden md:block"
-            alt="not found"
-          />
-        </div>
+        </Carousel>
       </div>
       {/* Cast */}
-      <div className="flex flex-row md:justify-between justify-around xl:pl-24 2xl:pl-32 lg:pl-10 md:pl-12 pl-4 xl:pr-24 2xl:pr-32 lg:pr-6 md:pr-10 pr-2 mt-16">
+      <div className="flex flex-row md:justify-between justify-around xl:pl-28 2xl:pl-32 lg:pl-20 md:pl-20 pl-4 xl:pr-28 2xl:pr-28 lg:pr-20 md:pr-20 pr-2 lg:mt-16 md:mt-6 mt-4">
         <div className="mainTitle lg:mb-11 mb-4">Featured Casts</div>
         <div className="flex flex-row py-3">
-          <div className="navsee pr-2">See more </div>
+          <div className="navsee pr-2">See more</div>
           <div className="w-5 h-5">
             <img src={rightArrow} alt="not Found" />
           </div>
         </div>
       </div>
-
-      <div className="flex flex-row justify-center">
-        <div>
-          <img
-            src={leftnav}
-            className="  mt-32  hidden md:block  lg:w-12 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4 "
-            alt="not found"
-          />
-        </div>
-        <div className="flex md:flex-row flex-col xl:space-x-20 lg:space-x-6 md:space-x-4 space-x-0">
+      <div className="flex">
+        <Carousel
+          breakPoints={breakPoints}
+          pagination={false}
+          renderArrow={myArrow}
+          className="mx-5"
+        >
           {allCast &&
             allCast.cast &&
-            allCast.cast.slice(0, 4).map((cdata) => (
+            allCast.cast.map((cdata) => (
               <div key={cdata.id}>
-                <div className="relative sm:flex-col xl:w-64 lg:w-56 md:w-40">
+                <div className="sm:flex-col xl:w-64 lg:w-48 md:w-36 w-48">
                   <img
                     src={`http://image.tmdb.org/t/p/w185/${cdata.profile_path}`}
-                    className="lg:h-96 md:h-72 w-64"
+                    className="xl:h-96 lg:h-72 md:h-64 w-64"
                     alt="not found"
                   />
                 </div>
-
-                <div className="insideTitle xl:w-64 lg:w-56 md:w-40 w-64">
+                <div className="insideTitle xl:w-64 lg:w-48 md:w-36 w-48">
                   {cdata.name}
                 </div>
               </div>
             ))}
-        </div>
-        <div>
-          <img
-            src={rightNav}
-            className="lg:w-10 xl:w-12 md:w-6 xl:mr-6 lg:mr-4 xl:ml-4   mt-32 hidden md:block"
-            alt="not found"
-          />
-        </div>
+        </Carousel>
       </div>
       {/* Footer */}
       <div className=" lg:mt-28 mt-10">
@@ -316,6 +277,19 @@ const Homedisplay = () => {
         </div>
       </div>
     </div>
+  );
+};
+const myArrow = ({ type, onClick, isEdge }) => {
+  const pointer =
+    type === "PREV" ? (
+      <img src={leftnav} className="mb-32" alt="not found" />
+    ) : (
+      <img src={rightNav} className="mb-32" alt="not found" />
+    );
+  return (
+    <button onClick={onClick} disabled={isEdge}>
+      {pointer}
+    </button>
   );
 };
 export default Homedisplay;
